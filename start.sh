6b7948dc591af5c917d29ae57086075581f7dcc9
majorz <zahari@resin.io>
2018-07-26 16:14:19 +0300

    Sleep intervals updated 2

--- start.sh
+++ start.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
-sleep 5s
+sleep 15s
+
+echo 'Unmanage WiFi interface'
 
 python -c "import NetworkManager; NetworkManager.NetworkManager.GetDeviceByIpIface('wlan0').Managed = False"
 
 sleep 5s
+
+echo 'Set ip address'
 
 ip addr flush dev wlan0
 ip addr add 192.168.42.1/24 dev wlan0
 
 sleep 5s
 
+echo 'Start dnsmasq & hostapd'
+
 systemctl unmask dnsmasq
 systemctl restart dnsmasq
 
 systemctl unmask hostapd
 systemctl restart hostapd
-
-sleep infinity
