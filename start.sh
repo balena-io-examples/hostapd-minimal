c17b92f24d1023c00f44da4ae7423941442fcb5f
majorz <zahari@resin.io>
2018-07-26 16:04:04 +0300

    Sleep intervals updated

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+sleep 5s
+
 python -c "import NetworkManager; NetworkManager.NetworkManager.GetDeviceByIpIface('wlan0').Managed = False"
+
+sleep 5s
 
 ip addr flush dev wlan0
 ip addr add 192.168.42.1/24 dev wlan0
 
-sleep 20s
+sleep 5s
 
 systemctl unmask dnsmasq
 systemctl restart dnsmasq
